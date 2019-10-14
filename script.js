12d516b086c380190d00f2af46add9306d979860
Chabrov <51097988+Chabrov@users.noreply.github.com>
2019-10-14 13:05:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ $(window).scroll(function(){
   $('a').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
+document.getElementById('bigBar') = bigBar;
+
+function fillNav() {
+  if(bigBar.style.backgroundColor = 'transparent') {
+    bigBar.style.backgroundColor = '#0D0D0D';
+} else if(bigBar.style.backgroundColor = '#0D0D0D') {
+    bigBar.style.backgroundColor = 'transparent';
+}
+}
