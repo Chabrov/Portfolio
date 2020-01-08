6fc28bc8e93bb5631a79dc8b02cc67e9d8b92cde
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-01-08 16:43:08 +0100

    switching design to b/w

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+// import simpleParallax from 'simple-parallax-js';
 $(window).scroll(function(){
   $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
   $('a').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
 
-
+//Making navbar transparent if on top, otherwise fill with color/
 document.getElementById('bigBar') = bigBar;
 
 function fillNav() {
   if(bigBar.style.backgroundColor = 'transparent') {
-    bigBar.style.backgroundColor = '#0D0D0D';
-} else if(bigBar.style.backgroundColor = '#0D0D0D') {
+    bigBar.style.backgroundColor = 'white';
+} else if(bigBar.style.backgroundColor = 'white') {
     bigBar.style.backgroundColor = 'transparent';
 }
-}
+};
+
+const background = document.getElementById("chabrowski");
+new simpleParallax(background)
